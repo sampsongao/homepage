b6e261f1e84bdd34ccb179cd7a1d7a81a52bf30a
Sampson Gao <sfgao@amazon.com>
2025-01-28 19:02:45 -0500

    Add simple message and colorful background

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,26 @@ export default function Home() {
   return (
     <ThemeContext.Provider value={themeContext}>
       <div className={darkModeOn ? "dark" : ""}>
-        <div className="h-screen bg-white dark:bg-black">
+        <div className="bg-white dark:bg-black">
           <NavBar />
 
+          <section className="relative h-[80vh]">
+            <div className="h-full w-screen">
+              <div className="h-full flex items-center justify-center">
+                <h1 className="text-3xl font-bold text-center text-black dark:text-white">
+                  Welcome to my home page!
+                </h1>
+              </div>
+            </div>
+
+            <div className="absolute h-full w-full bg-linear-[25deg,red_5%,yellow_60%,lime_90%,teal]" style={{
+              filter: "blur(100px)",
+              WebkitFilter: "blur(100px)",
+              backgroundImage: "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)"
+            }}>
+
+            </div>
+          </section>
         </div>
       </div>
     </ThemeContext.Provider>
